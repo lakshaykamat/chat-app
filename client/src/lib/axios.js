a45b9d6a781300bf0e95b9b199bc054578dec232
Lakshay Kamat <lakshaykamat2048@gmail.com>
2024-06-28 13:00:17 +0530

    bn

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,19 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Create an Axios instance with default settings
 const axiosInstance = axios.create({
-  baseURL: 'https://chat-app-squi.onrender.com/api/v1', // Your API base URL
+  baseURL: "https://chat-app-squi.onrender.com/api/v1", // Your API base URL
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   },
 });
 
 // Add a request interceptor to include the auth token
 axiosInstance.interceptors.request.use(
   (config) => {
-    const user = JSON.parse(localStorage.getItem('authToken'));
+    const user = JSON.parse(localStorage.getItem("authToken"));
     if (user) {
-      config.headers['Authorization'] = `Bearer ${user.token}`;
+      config.headers["Authorization"] = `Bearer ${user.token}`;
     }
     return config;
   },
