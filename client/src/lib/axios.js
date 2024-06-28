4e865830f64b0f9ddec24928f34b1df5e02a2301
Lakshay Kamat <lakshaykamat2048@gmail.com>
2024-06-28 12:31:15 +0530

    FIx err

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with default settings
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api/v1', // Your API base URL
+  baseURL: 'https://chat-app-squi.onrender.com/api/v1', // Your API base URL
   headers: {
     'Content-Type': 'application/json',
   },
